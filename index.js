0c8e169c8c176f91e379939cbd0cec4f689b4094
PunitDh <cas012115@coderacademy.edu.au>
2022-02-10 16:08:06 +1100

    Change batch size back to 100

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.post("/api/batchfetch", (req, res) => {
   console.log(`Started POST /api/batchfetch at`, new Date().toLocaleString());
 
   const emailIDs = req.body.emailIDs.split(",");
-  const emailBatches = chunkArray(emailIDs, 50);
+  const emailBatches = chunkArray(emailIDs, 100);
   console.log(emailBatches);
   const options = querystring.stringify({
     format: "metadata",
